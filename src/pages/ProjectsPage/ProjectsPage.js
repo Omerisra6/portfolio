d5548bf4a6acbeee0615b528b71e8b7c5ab641ac
Omerisra06 <Omerisraeli6@gmail.com>
2023-06-05 11:15:43 +0300

    New: Projects page added

--- src/pages/ProjectsPage/ProjectsPage.js
+++ src/pages/ProjectsPage/ProjectsPage.js
@@ -1,7 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+import ProjectCard from './components/ProjectCard'
+import projectsData from '../../assets/projects.json';
+
+const StyledProjectsPage = styled.div`
+    width: 100%;
+    max-width: 1280px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-self: center;
+    gap: 1.5em;
+    padding: 25vh 0 15vh 0;
+    color: var( --theme-light-text-color );
+    
+    .projects-description{
+        text-align: center;
+    }
+
+    .project-cards-container{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: stretch;
+        flex-wrap: wrap;
+        gap: 2em;
+    }
+`
 
 export default function ProjectsPage() {
+
     return (
-        <div>ProjectsPage</div>
+        <StyledProjectsPage>
+            <h1 className='my-projects-text'>My Projects</h1>
+            <span className='projects-description'>Selected projects that will reflect my knowladge</span>
+
+            <div className='project-cards-container'>
+               { projectsData.map( project => {
+                    return <ProjectCard project={ project }/>
+               })}
+            </div>
+        </StyledProjectsPage> 
+
     )
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,16 @@
-export function getRandomInt(min, max) {
+export function getRandomInt(min, max) 
+{
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function slugify( str )
+{
+    return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^\w\s-]/g, '')
+    .replace(/[\s_-]+/g, '-')
+    .replace(/^-+|-+$/g, '');
 }
