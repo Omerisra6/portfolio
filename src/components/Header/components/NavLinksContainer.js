96450d9fb96df5a6a8d03256c6d11d0f6f049b04
Omerisra06 <Omerisraeli6@gmail.com>
2023-06-26 21:36:09 +0300

    Fix: Referer header fixed

--- src/components/Header/components/NavLinksContainer.js
+++ src/components/Header/components/NavLinksContainer.js
@@ -23,7 +23,7 @@ export default function NavLinksContainer() {
         <div>My Projects</div>
       </a>
 
-      <a className="link" href="https://drive.google.com/file/d/1KTHzE_aEjRa1y1EC5zLQg_w2Idz3Qjj4/view?usp=sharing" target="_blank">
+      <a className="link" href="https://drive.google.com/file/d/1KTHzE_aEjRa1y1EC5zLQg_w2Idz3Qjj4/view?usp=sharing" target="_blank" rel="noreferrer">
         <div>My Resume</div>
       </a>
     </StyledNavLinksContainer>
